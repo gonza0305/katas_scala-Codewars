2f3624e545ea26e0b8ed77d390eea94af1db1044
Gonzalo Lencina Lorenzon <gonza0305@hotmail.com>
2021-04-14 11:54:24 +0200

    Create meeting.scala

---
+++ codewars-6kyu/meeting.scala
@@ -0,0 +1,12 @@
+object Meeting {
+
+  def meeting(s: String): String = {
+    val upperCase = s.toUpperCase
+    val list = upperCase.split(";")
+    val list2 = list.map(data => (data.split(":")(0),data.split(":")(1)))
+    val list3 = list2.sortBy(data => (data._2,data._1))
+    val list4 = list3.map(d=>(d._2," " + d._1))
+    list4.mkString("")
+    // your code
+  }
+}
