c502f1b483a742ee25546ace1d261697268e892a
Gonzalo Lencina Lorenzon <gonza0305@hotmail.com>
2021-04-14 12:25:09 +0200

    Update and rename backwards_read_primes.scala to BackwardsReadPrimes.scala

--- codewars-6kyu/BackwardsReadPrimes.scala
+++ codewars-6kyu/BackwardsReadPrimes.scala
@@ -1,3 +1,6 @@
+
+// LINK : https://www.codewars.com/kata/5539fecef69c483c5a000015
+
 object Solution {
   def backwardsPrime(start: Long, nd: Long): String =
     (start to nd).toList.filter(u=> isPrime(u.toString.reverse.toLong) && isPrime(u) && u.toString.reverse!=u.toString).mkString(",")
