f7a2a94b39fee1652373c5409b884fb3e1d2a332
Gonzalo Lencina Lorenzon <gonza0305@hotmail.com>
2021-04-14 12:01:09 +0200

    Create find_the_parity_outlier.scala

---
+++ codewars-6kyu/find_the_parity_outlier.scala
@@ -0,0 +1,12 @@
+object Parity {
+
+  def findOutlier(integers: List[Int]): Int = {
+    if(isEven(integers.head) && isEven(integers(1))) return integers.filter(x => !isEven(x)).head
+    if(!isEven(integers.head) && !isEven(integers(1))) integers.filter(x => isEven(x)).head
+    else {
+        if(isEven(integers(2))) integers.filter(x => !isEven(x)).head
+        else integers.filter(x => isEven(x)).head
+    }
+  }
+  def isEven(n: Long): Boolean = n%2==0
+}
