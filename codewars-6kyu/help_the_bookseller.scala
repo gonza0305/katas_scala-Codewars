8d05e8333c9495b06dbc82724c5262f91c6281ec
Gonzalo Lencina Lorenzon <gonza0305@hotmail.com>
2021-04-14 11:53:23 +0200

    Create help_the_bookseller.scala

---
+++ codewars-6kyu/help_the_bookseller.scala
@@ -0,0 +1,25 @@
+import scala.collection.mutable.ArrayBuffer
+
+object StockList {
+  def stockSummary(lstOfArt: Array[String], lstOfCat: Array[String]): String = {
+    val aux2: ArrayBuffer[String] = new ArrayBuffer[String]()
+    if(lstOfArt.isEmpty || lstOfCat.isEmpty) ""
+    else{
+      lstOfCat.foreach( cat => {
+        val aux: ArrayBuffer[Int] = new ArrayBuffer[Int]()
+        lstOfArt.foreach(elem => {
+          if(elem.take(1).equals(cat)){
+
+            val quant = elem.split(" ")(1).toInt
+            aux.append(quant)
+
+          }
+        })
+        if(aux2.isEmpty)aux2.append("("+cat+" "+":"+" "+aux.sum+")")
+        else aux2.append(" - ("+cat+" "+":"+" "+aux.sum+")")
+      })
+
+      aux2.mkString("")
+    }
+  }
+}
