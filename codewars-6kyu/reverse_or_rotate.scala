a5b97280eca63ca40802026ea1b63da18ab0bd51
Gonzalo Lencina Lorenzon <gonza0305@hotmail.com>
2021-04-14 11:50:46 +0200

    Create reverse_or_rotate.scala

---
+++ codewars-6kyu/reverse_or_rotate.scala
@@ -0,0 +1,25 @@
+import scala.collection.mutable.ArrayBuffer
+
+object RevRot {
+
+  def revRot(strng: String, sz: Int): String = {
+    // your code
+    if(sz <= 0 || strng.isEmpty || sz > strng.length) ""
+    else{
+      val newString : ArrayBuffer[String] = ArrayBuffer()
+      val substrings = strng.grouped(sz)
+
+      substrings.foreach(str => {
+        if(str.length >= sz){
+          val number = str.map( digit => digit*digit*digit).sum
+          if(number % 2 == 0) {
+            newString.append(str.reverse)
+          }else{
+            newString.append(str.substring(1) + str.substring(0, 1))
+          }
+        }
+      })
+      newString.mkString("")
+    }
+  }
+}
